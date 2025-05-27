080e5f49e939f2eb0f434597d893e815c22d688d
Tamim <tamimyousefi023@gmail.com>
2025-05-27 20:11:08 +0330

    feat(SearchPanel): search panel for filtering menu items

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero/Hero";
+import { SearchPanel } from "@/components/searchPanel/SearchPanel";
 import { TLanguages } from "@/utils/getTranslation";
 interface PageProps {
   params: Promise<{
@@ -8,8 +9,9 @@ interface PageProps {
 export default async function Home({ params }: PageProps) {
   const { locale } = await params;
   return (
-    <main className="container max-w-[1440px] mx-auto p-6 md:px-16 md:py-8 flex ">
+    <main className="container max-w-[1440px] mx-auto p-6 md:px-16 md:py-8 flex flex-col">
       <Hero locale={locale} />
+      <SearchPanel />
     </main>
   );
 }
