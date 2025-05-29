bb860a0dc103e5a5107e63ef73a9818a667eff82
Tamim <tamimyousefi023@gmail.com>
2025-05-29 15:04:36 +0330

    feat(Pagintaion): creating pagination component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/app/[locale]/+components/Hero";
 import { SearchPanel } from "@/app/[locale]/+components/searchPanel/SearchPanel";
 import { TLanguages } from "@/utils/getTranslation";
+import Menu from "./+components/Menu";
+import { Pagination } from "./+components/Pagintaion";
 interface PageProps {
   params: Promise<{
     locale: TLanguages;
@@ -12,6 +14,8 @@ export default async function Home({ params }: PageProps) {
     <>
       <Hero locale={locale} />
       <SearchPanel locale={locale} />
+      <Menu locale={locale} />
+      <Pagination pageCount={12} />
     </>
   );
 }
