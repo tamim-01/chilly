7fa9c52d0ff57983ff4d19fc22554c3778eaeb08
Tamim <tamimyousefi023@gmail.com>
2025-06-12 15:30:06 +0330

    feat(dash-layout): route protection for dashboard

--- src/app/dash/layout.tsx
+++ src/app/dash/layout.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Itim, Irish_Grover } from "next/font/google";
 import "../globals.css";
-import { TLanguages } from "@/utils/getTranslation";
 import { ToastProvider } from "@/components/UI/ToastProvider";
+import { useEffect, useState } from "react";
+import Fetch from "@/utils/Fetch";
+import { usePathname, useRouter } from "next/navigation";
+import LoadingSpinner from "@/components/UI/Spinner";
 
 const geistSans = Itim({
   weight: "400",
@@ -17,21 +21,57 @@ const geistIrish = Irish_Grover({
 
 interface LayoutProps {
   children: React.ReactNode;
-  params: Promise<{
-    locale: TLanguages;
-  }>;
 }
-export default async function LocaleLayout({ children, params }: LayoutProps) {
-  const { locale } = await params;
-  return (
-    <html lang={locale} dir={locale === "fa" ? "rtl" : "ltr"}>
-      <body
-        className={`${geistSans.variable} ${geistIrish.variable}  antialiased`}
-      >
-        <main className="container max-w-[1440px] mx-auto p-6 md:px-16 md:py-8 flex flex-col">
-          <ToastProvider> {children}</ToastProvider>
-        </main>
-      </body>
-    </html>
-  );
+export default function DashLayout({ children }: LayoutProps) {
+  const [auth, setAuth] = useState(false);
+  const r = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await Fetch.get({ url: "/admin" }).then((res) => {
+          if (res.status === "success") {
+            localStorage.setItem("user", res.result.username);
+            localStorage.setItem("role", res.result.role);
+            if (path === "/dash/login") {
+              r.push("/dash");
+            }
+            setAuth(true);
+          } else {
+            r.push("/dash/login");
+          }
+        });
+      } catch (error) {
+        setAuth(false);
+        console.log(error);
+      }
+    };
+    checkAuth();
+  }, [r, path]);
+  if (!auth && path !== "/dash/login") {
+    return (
+      <html>
+        <body
+          className={`${geistSans.variable} ${geistIrish.variable}  antialiased`}
+        >
+          <main className="container max-w-[1440px] mx-auto p-6 md:px-16 md:py-8 flex flex-col justify-center items-center">
+            <LoadingSpinner />
+          </main>
+        </body>
+      </html>
+    );
+  }
+  if (auth || path === "/dash/login")
+    return (
+      <html>
+        <body
+          className={`${geistSans.variable} ${geistIrish.variable}  antialiased`}
+        >
+          <main className="container max-w-[1440px] mx-auto p-6 md:px-16 md:py-8 flex flex-col">
+            <ToastProvider>{children}</ToastProvider>
+          </main>
+        </body>
+      </html>
+    );
 }
