9847ce4cddb0bde8eb62540c8027885c1c608a8d
Tamim <tamimyousefi023@gmail.com>
2025-06-30 00:06:03 +0330

    feat(dependency-field): added depnendency field component

---
+++ src/app/[locale]/dash/+components/forms/DependencyField.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import Button from "@/components/UI/Button";
+import Select from "@/components/UI/inputs/Select";
+import TextInput from "@/components/UI/inputs/TextInput";
+import Skeleton from "@/components/UI/skeleton";
+import useFetchedData from "@/hooks/useFetchedData";
+import { useEffect, useState } from "react";
+
+export default function DependencyField({
+  onChange,
+  error,
+}: {
+  onChange: React.Dispatch<
+    React.SetStateAction<Array<{ label: string; value: number }> | null>
+  >;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: any;
+}) {
+  const { data, loading } = useFetchedData<InventoryItem[]>(`api/inventory`);
+  const [selectedDep, setSelectedDep] = useState<
+    Array<{ label: string; value: number }>
+  >([]);
+
+  const options = data?.map((i) => {
+    return { value: i.title, label: i.title };
+  });
+  function selectedValuesChangeHandler(selectedValues: string | string[]) {
+    if (Array.isArray(selectedValues)) {
+      selectedValues.forEach((selected) => {
+        const isNew = !selectedDep.map((i) => i["label"]).includes(selected);
+        if (isNew) {
+          setSelectedDep([...selectedDep, { label: selected, value: 10 }]);
+        } else {
+        }
+      });
+    }
+  }
+  useEffect(() => {
+    if (onChange) {
+      onChange(selectedDep);
+    }
+  }, [selectedDep, onChange]);
+  return (
+    <section className="w-full">
+      {loading ? (
+        <div className="my-6">
+          <p className=" text-[18px]">Food dependency</p>
+          <Skeleton width="354px" height="53px" rounded="xl" />
+        </div>
+      ) : (
+        options && (
+          <>
+            <Select
+              fullWidth
+              options={options}
+              value={selectedDep.map((i) => i.label)}
+              label="Food dependency"
+              className="my-6 text-[18px] max-w-[354px]"
+              multiple
+              searchable
+              onChange={selectedValuesChangeHandler}
+              error={error?.message ? true : false}
+              errorMessage={error?.message}
+            />
+            <section className="flex flex-row flex-wrap gap-6">
+              {selectedDep.map((dep, index) => (
+                <div key={dep.label} className="flex flex-row ">
+                  <TextInput
+                    type="number"
+                    label={dep.label}
+                    step={10}
+                    min={10}
+                    value={selectedDep[index].value}
+                    icon={
+                      data
+                        ?.map((d) => {
+                          return { unit: d.unit, label: d.title };
+                        })
+                        .find((i) => i.label === dep.label)?.unit
+                    }
+                    iconPosition="right"
+                    onChange={(v) => {
+                      const value = isNaN(v.target.valueAsNumber)
+                        ? 0
+                        : v.target.valueAsNumber;
+                      const newObj = {
+                        label: selectedDep[index].label,
+                        value: value,
+                      };
+                      selectedDep[index] = newObj;
+                      setSelectedDep([...selectedDep]);
+                    }}
+                  />
+                  <Button
+                    variant="secondary"
+                    className="rounded-full py-2 mt-8 ml-2 "
+                    onClick={() => {
+                      const filterDeps = selectedDep.filter(
+                        (d) => d.label !== dep.label
+                      );
+                      setSelectedDep([...filterDeps]);
+                    }}
+                  >
+                    X
+                  </Button>
+                </div>
+              ))}
+            </section>
+          </>
+        )
+      )}
+    </section>
+  );
+}
