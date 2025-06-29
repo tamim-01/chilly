4f0119ab28416133bb7601a48079ce681b21e9a9
Tamim <tamimyousefi023@gmail.com>
2025-06-30 00:00:38 +0330

    fix(app.d): change isActive to active

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,5 +9,15 @@ interface MenuItem {
   payment_type: Price;
   description: string;
   spicy: boolean;
+  isActive: boolean;
   image_urls: string[];
+}
+interface InventoryItem {
+  id: number;
+  admin_id: number;
+  title: string;
+  unit: string;
+  available_quantity: number;
+  category: string;
+  publish_date: string;
 }
