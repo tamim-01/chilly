ec27b84f7fb8a81665a98428aa8e61ad91e70905
Tamim <tamimyousefi023@gmail.com>
2025-07-02 00:00:59 +0330

    feat(edit): added edit item form

--- src/app/[locale]/dash/edit/[id]/page.tsx
+++ src/app/[locale]/dash/edit/[id]/page.tsx
@@ -1,7 +1,31 @@
+import Fetch from "@/utils/Fetch";
+import Button from "@/components/UI/Button";
+import Link from "next/link";
+import EditItemForm from "../../+components/forms/Edit/EditItemForm";
+
 interface PageProps {
   params: Promise<{ id: number }>;
 }
 export default async function page({ params }: PageProps) {
   const { id } = await params;
-  return <div>{id}</div>;
+  const result = await Fetch.get({ url: `/menu/item/${id}` });
+  if (result.status === "success") {
+    return (
+      <>
+        <h2 className="text-4xl mt-8 mb-16 w-full text-center ">
+          Edit menu item
+        </h2>
+        <EditItemForm itemId={id} />
+      </>
+    );
+  } else {
+    <section className="flex flex-col h-[600px] justify-center items-center space-y-6">
+      <h2 className="text-4xl">Failed to fetch item data!</h2>
+      <div className="flex flex-row gap-5">
+        <Link href={"/"}>
+          <Button variant="secondary">Back home</Button>
+        </Link>
+      </div>
+    </section>;
+  }
 }
