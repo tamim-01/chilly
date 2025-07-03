140a880f11e545a32466997fc4d4e08d5a2f24e3
Tamim <tamimyousefi023@gmail.com>
2025-07-03 16:51:49 +0330

    style(addnew): style changes

--- src/components/Common/AddNew.tsx
+++ src/components/Common/AddNew.tsx
@@ -4,8 +4,8 @@ import { TLanguages } from "@/utils/getTranslation";
 
 export default function AddNew({ locale }: { locale: TLanguages }) {
   return (
-    <section className="md:p-8 mt-12">
-      <Link href={`/${locale}/dash/add`}>
+    <section className="md:p-8 mt-12 w-fit">
+      <Link href={`/${locale}/dash/add`} className="w-fit">
         <Button
           variant="secondary"
           size="lg"
